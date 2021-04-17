1f9fecd22b1947fe01d2be855a9c73579bf0f63a
Stefan Haun <tux@netz39.de>
2021-04-17 19:04:44 +0200

    Add a presence color class

--- presence.py
+++ presence.py
@@ -24,3 +24,24 @@ class Presence:
     @presence.setter
     def presence(self, presence):
         self._presence = presence
+
+
+class PresenceColor:
+    absent_color_rgba = [77 / 256, 77 / 256, 76 / 256, 1]
+    present_color_rgba = [0 / 256, 163 / 256, 86 / 256, 1]
+#    away_color_rgba = [249 / 256, 176 / 256, 0 / 256, 1]
+    away_color_rgba = [68 / 256, 53 / 256, 126 / 256, 1]
+    occupied_color_rgba = [228 / 256, 5 / 256, 41 / 256, 1]
+
+    @staticmethod
+    def color_for(value):
+        if value == "absent":
+            return PresenceColor.absent_color_rgba
+        elif value == "present":
+            return PresenceColor.present_color_rgba
+        elif value == "away":
+            return PresenceColor.away_color_rgba
+        elif value == "occupied":
+            return PresenceColor.occupied_color_rgba
+
+        return None
