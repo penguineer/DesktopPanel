ff56c56793f5d283d7e49d10016df4306288233c
Stefan Haun <mail@tuxathome.de>
2022-09-17 15:52:36 +0200

    :hammer: Create independent thermometer widget and revamp property handling
    Make the widget more reusable and hopefully fix "hanging" level display.

--- temperature.py
+++ temperature.py
@@ -1,11 +1,21 @@
 """ Module for temperature display """
-
+from kivy import Logger
 from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.properties import StringProperty, NumericProperty, ColorProperty, ObjectProperty, ListProperty, DictProperty
 from kivy.uix.relativelayout import RelativeLayout
 
 import time
+
+
+class Colors:
+    # Base color definitions
+    COLOR_BLACK = [0, 0, 0, 1]
+    COLOR_WHITE = [1, 1, 1, 1]
+    COLOR_GREY = [77 / 256, 77 / 256, 76 / 256, 1]
+    COLOR_GREEN = [0 / 256, 163 / 256, 86 / 256, 1]
+    COLOR_YELLOW = [249 / 256, 176 / 256, 0 / 256, 1]
+    COLOR_RED = [228 / 256, 5 / 256, 41 / 256, 1]
 
 
 Builder.load_string("""
@@ -47,104 +57,45 @@ class TemperaturePanel(RelativeLayout):
 
 
 Builder.load_string("""
+#:import Colors temperature.Colors
+
 <TemperatureView>:    
     BoxLayout:
         orientation: 'vertical'
-        size: 40, 76
+        size: 40, 95
         size_hint: None, None
 
         Label:
             text_size: root.width, None    
-            size: self.texture_size
+            #size: self.texture_size
+            size: 40, 12
             size_hint: None, None
             #text: root.label
             text: root.conf.label if root.conf else '--'
             font_size: 10
             halign: 'center'
-            color: root.label_color
+            color: Colors.COLOR_RED if root.value_error else \
+                   Colors.COLOR_GREY if root._temp is None else \
+                   Colors.COLOR_WHITE
 
-        Label:
-            size: root.width, 64
-            size_hint: None, None
-            canvas:
-                # Background
-                Color:
-                    rgba: root.COLOR_GREY
-                Ellipse: 
-                    pos: self.center_x-13, -5
-                    size: 26, 26
-                Color:
-                    rgba: root.COLOR_BLACK
-                Ellipse: 
-                    pos: self.center_x-12, -4
-                    size: 24, 24                    
-                Color:
-                    rgba: root.COLOR_GREY
-                Rectangle:
-                    pos: self.center_x-6, 19
-                    size: 12, 56
-            
-                # Level 1
-                Color:
-                    rgba: root.COLOR_BLACK if root.level < 1 else root.COLOR_GREEN
-                Rectangle:
-                    pos: self.center_x-5, 20
-                    size: 10, 10    
-                # Level 2
-                Color:
-                    rgba: root.COLOR_BLACK if root.level < 2 else root.COLOR_GREEN
-                Rectangle:
-                    pos: self.center_x-5, 31
-                    size: 10, 10    
-                # Level 3
-                Color:
-                    rgba: root.COLOR_BLACK if root.level < 3 else root.COLOR_GREEN
-                Rectangle:
-                    pos: self.center_x-5, 42
-                    size: 10, 10    
-                # Level 4
-                Color:
-                    rgba: root.COLOR_BLACK if root.level < 4 else root.COLOR_YELLOW
-                Rectangle:
-                    pos: self.center_x-5, 53
-                    size: 10, 10    
-                # Level 5
-                Color:
-                    rgba: root.COLOR_BLACK if root.level < 5 else root.COLOR_RED
-                Rectangle:
-                    pos: self.center_x-5, 64
-                    size: 10, 10    
-
-        Label:
-            size: 40, 14
-            size_hint: None, None
-            text: root.temp
-            font_size: 12   
-            text_size: root.width, None    
-            size: self.texture_size
-            halign: 'center'
-            color: root.temp_color
-
+        ThermometerWidget:
+            limit_min: root.conf.get("min", 0) if root.conf else 0
+            limit_warn: root.conf.get("warn", 0) if root.conf else 0
+            limit_alarm: root.conf.get("alarm", 0) if root.conf else 0
+            temperature: root._temp
 """)
 
 
 class TemperatureView(RelativeLayout):
-    COLOR_BLACK = [0, 0, 0, 1]
-    COLOR_WHITE = [1, 1, 1, 1]
-    COLOR_GREY = [77 / 256, 77 / 256, 76 / 256, 1]
-    COLOR_GREEN = [0 / 256, 163 / 256, 86 / 256, 1]
-    COLOR_YELLOW = [249 / 256, 176 / 256, 0 / 256, 1]
-    COLOR_RED = [228 / 256, 5 / 256, 41 / 256, 1]
-
     MEASUREMENT_TIMEOUT = 60  # [s]
-
-    temp = StringProperty("--°")
-    level = NumericProperty(0)
-    label_color = ColorProperty(COLOR_GREY)
-    temp_color = ColorProperty(COLOR_GREY)
 
     mqttc = ObjectProperty(None)
     conf = DictProperty(None)
+    value_error = ObjectProperty(None, allownone=True)
+
+    _label_color = ColorProperty(Colors.COLOR_GREY)
+
+    _temp = NumericProperty(None, allownone=True)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -155,13 +106,10 @@ class TemperatureView(RelativeLayout):
         # (Scheduling does not have to be super precise, so we chose a rather long interval.)
         Clock.schedule_interval(lambda dt: self._check_measurement_age(), 10)
 
-        self.bind(conf=self._on_conf)
-        self.bind(mqttc=self._on_mqttc)
-
-    def _on_conf(self, _instance, _conf: list) -> None:
+    def on_conf(self, _instance, _conf: list) -> None:
         self._update_mqtt()
 
-    def _on_mqttc(self, _instance, _mqttc) -> None:
+    def on_mqttc(self, _instance, _mqttc) -> None:
         self._update_mqtt()
 
     def _update_mqtt(self):
@@ -172,60 +120,20 @@ class TemperatureView(RelativeLayout):
         self.mqttc.subscribe(topic, self._mqtt_callback)
 
     def _mqtt_callback(self, _client, _userdata, message):
+        payload = message.payload.decode("utf-8")
+        Clock.schedule_once(lambda dt: self._update_temperature(payload))
+
+    def _update_temperature(self, payload):
         # We received a measurement
         self.measure_instant = time.time()
 
-        payload = message.payload.decode("utf-8")
         try:
-            temp_f = float(payload)
-
-            self.label_color = TemperatureView.COLOR_WHITE
-            self.temp_color = TemperatureView.COLOR_GREY
-            self.temp = f"%d°" % round(temp_f)
-
-            self._calculate_level(temp_f)
+            self.value_error = None
+            self._temp = float(payload)
         except ValueError as e:
-            print(e)
-            self.temp = "EE"
-            self.label_color = TemperatureView.COLOR_RED
-            self.temp_color = TemperatureView.COLOR_RED
-
-    def _calculate_level(self, temp_f):
-        # Any ValueError from float conversions will be caught by _mqtt_callback
-
-        # Level 5 if past alarm temperature
-        alarm_s = self.conf.get("alarm", None)
-        if alarm_s:
-            alarm = float(alarm_s)
-            if temp_f >= alarm:
-                self.level = 5
-                self.label_color = TemperatureView.COLOR_RED
-                self.temp_color = TemperatureView.COLOR_RED
-
-                return
-
-        # Level 4 if past warn temperature
-        warn_s = self.conf.get("warn", None)
-        if warn_s:
-            warn = float(warn_s)
-            if temp_f >= warn:
-                self.level = 4
-                self.temp_color = TemperatureView.COLOR_YELLOW
-
-                return
-
-        # everything else depends on the temperature limits
-        min_s = self.conf.get("min", None)
-
-        if min_s and warn_s:
-            t_min = float(min_s)
-            t_max = float(warn_s)
-
-            # Distribute the rest over the levels 0 to 3
-            # Note that there is int-like cut-off instead of rounding!
-            level = (temp_f - t_min) / (t_max - t_min) * 4
-            self.level = level
-            self.temp_color = TemperatureView.COLOR_GREEN
+            Logger.error(e)
+            self.value_error = e
+            self._temp = None
 
     def _check_measurement_age(self):
         age = time.time() - self.measure_instant \
@@ -233,7 +141,138 @@ class TemperatureView(RelativeLayout):
             else TemperatureView.MEASUREMENT_TIMEOUT + 1
 
         if age > TemperatureView.MEASUREMENT_TIMEOUT:
-            self.temp = "--"
-            self.temp_color = TemperatureView.COLOR_GREY
-            self.label_color = TemperatureView.COLOR_GREY
-            self.level = 0
+            self._temp = None
+
+
+Builder.load_string("""
+#:import Colors temperature.Colors
+
+<ThermometerWidget>:    
+    BoxLayout:
+        orientation: 'vertical'
+        size: 40, 85
+        size_hint: None, None
+
+        Label:
+            size: root.width, 64
+            size_hint: None, None
+            canvas:
+                # Background
+                Color:
+                    rgba: Colors.COLOR_GREY
+                Ellipse: 
+                    pos: self.center_x-13, 0
+                    size: 26, 26
+                Color:
+                    rgba: Colors.COLOR_BLACK
+                Ellipse: 
+                    pos: self.center_x-12, 1
+                    size: 24, 24                    
+                Color:
+                    rgba: Colors.COLOR_GREY
+                Rectangle:
+                    pos: self.center_x-6, 24
+                    size: 12, 56
+
+                # Level 1
+                Color:
+                    rgba: Colors.COLOR_GREEN if root._level and root._level >= 1 else Colors.COLOR_BLACK
+                Rectangle:
+                    pos: self.center_x-5, 25
+                    size: 10, 10    
+                # Level 2
+                Color:
+                    rgba: Colors.COLOR_GREEN if root._level and root._level >= 2 else Colors.COLOR_BLACK
+                Rectangle:
+                    pos: self.center_x-5, 36
+                    size: 10, 10    
+                # Level 3
+                Color:
+                    rgba: Colors.COLOR_GREEN if root._level and root._level >= 3 else Colors.COLOR_BLACK
+                Rectangle:
+                    pos: self.center_x-5, 47
+                    size: 10, 10    
+                # Level 4
+                Color:
+                    rgba: Colors.COLOR_YELLOW if root._level and root._level >= 4 else Colors.COLOR_BLACK
+                Rectangle:
+                    pos: self.center_x-5, 58
+                    size: 10, 10    
+                # Level 5
+                Color:
+                    rgba: Colors.COLOR_RED if root._level and root._level >= 5 else Colors.COLOR_BLACK
+                Rectangle:
+                    pos: self.center_x-5, 69
+                    size: 10, 10    
+
+        Label:
+            pos: 10, 50
+            size: 40, 26  # The height moves the text up without needing to define a padding label
+            size_hint: None, None
+            text: root._temp_label
+            font_size: 12   
+            text_size: root.width, None    
+            halign: 'center'
+            color: root._temp_color
+""")
+
+
+class ThermometerWidget(RelativeLayout):
+    limit_min = NumericProperty(0)
+    limit_warn = NumericProperty(0)
+    limit_alarm = NumericProperty(0)
+
+    temperature = NumericProperty(None, allownone=True)
+
+    _temp_label = StringProperty("--°")
+    _level = NumericProperty(None, allownone=True)
+    _temp_color = ColorProperty(Colors.COLOR_GREY)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.bind(on_limit_min=lambda _s, _i, _v: self._populate_level())
+        self.bind(on_limit_warn=lambda _s, _i, _v: self._populate_level())
+        self.bind(on_limit_alarm=lambda _s, _i, _v: self._populate_level())
+
+    def on_temperature(self, _instance, _value):
+        self._populate_label()
+        self._populate_level()
+
+    def _populate_label(self):
+        self._temp_label = f"%d°" % round(self.temperature) if self.temperature is not None else "--°"
+
+    def _populate_level(self):
+        # Level 0 if there is no temperature
+        if self.temperature is None:
+            self._level = None
+
+            return
+
+        # Level 5 if past alarm temperature
+        if self.temperature >= self.limit_alarm:
+            self._level = 5
+
+            return
+
+        # Level 4 if past warn temperature
+        if self.temperature >= self.limit_warn:
+            self._level = 4
+
+            return
+
+        # Distribute the rest over the levels 0 to 3
+        # Note that there is int-like cut-off instead of rounding!
+        t_range = max(0, self.limit_warn - self.limit_min)
+        level = (self.temperature - self.limit_min) / t_range * 4 if t_range else 0
+        self._level = max(0, level)  # avoid negative values for the level
+
+    def on__level(self, _instance, _value):
+        if self._level is None:
+            self._temp_color = Colors.COLOR_GREY
+        elif self._level == 5:
+            self._temp_color = Colors.COLOR_RED
+        elif self._level == 4:
+            self._temp_color = Colors.COLOR_YELLOW
+        else:
+            self._temp_color = Colors.COLOR_GREEN
